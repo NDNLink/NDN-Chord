680742985644b1d81438f176965b4d0737a879de
Alan <61774141+Alanshaw2048@users.noreply.github.com>
2020-10-14 15:10:56 +0800

    Update callbacks_list.py

--- src/applications/bindings/callbacks_list.py
+++ src/applications/bindings/callbacks_list.py
@@ -1,3 +1,4 @@
+
 callback_classes = [
     ['ns3::ObjectBase *', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty'],
     ['void', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty', 'ns3::empty'],
